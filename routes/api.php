89cb14ed75b88a4c9f1d9b13016ead6612e4b455
Maria <salemmaria2005@gmail.com>
2026-01-24 15:51:45 +0800

    api endpoint testing

--- routes/api.php
+++ routes/api.php
@@ -6,7 +6,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
-
-Route::get('/test', function (Request $request) {
-    return "test endpoint";
+Route::get('/test', function (Request $request) { 
+    return response()->json([ 'name' => 'Maria Salem', 'section' => 'BSCS 601', 'fav_song' => 'Yahya - keepyousafe']); 
 });
