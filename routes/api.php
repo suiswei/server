b1d70238df6a1a89ef4c420cd0325f33dd37ad82
njcs11 <jasphertadlan@gmail.com>
2026-01-24 15:46:53 +0800

    Lab1

--- routes/api.php
+++ routes/api.php
@@ -8,5 +8,9 @@ Route::get('/user', function (Request $request) {
 })->middleware('auth:sanctum');
 
 Route::get('/test', function (Request $request) {
-    return "test endpoint";
+    return response()->json([
+        'name' => 'Jaspher Lloyd Tadlan',
+        'section' => 'BSCS 601',
+        'fav_song' => 'Here Without You'
+    ]);
 });
